457f13152ebc293b788a91ec5d2c2452c2766b93
Misael <misael.domingo@hotmail.com>
2017-10-28 20:23:59 -0400

    df

--- CASH_Project/Global.asax.cs
+++ CASH_Project/Global.asax.cs
@@ -13,6 +13,8 @@ namespace CASH
         protected void Application_Start()
         {
 
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
